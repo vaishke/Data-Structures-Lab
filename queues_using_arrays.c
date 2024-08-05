aa68517c9ae848d0fbf50717927bc7427197b991
Vaishnavik1512 <165873721+Vaishnavik1512@users.noreply.github.com>
2024-08-05 10:22:23 +0530

    Create queues_using_arrays.c

---
+++ queues_using_arrays.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define SIZE 5
+
+int front = -1;
+int rear = -1;
+int queue[SIZE];
+
+bool isEmpty();
+void Insert(int);
+void Enqueue();
+void Dequeue();
+void Display();
+
+int main()
+{
+	int choice;
+	while(1)
+	{
+        printf("Queue Operations: \n1. Enqueue \n2. Dequeue \n3. Display \n4. Exit \n");
+		printf("Enter your choice: ");
+		scanf("%d", &choice);
+		
+		switch(choice)
+		{
+			case 1: Enqueue();
+					break;
+			
+			case 2: Dequeue();
+					break;
+			
+			case 3: Display();
+					break;
+			
+			case 4: exit(0);
+			
+			default: printf("Enter relevant number.\n");
+		}
+		
+	}
+}
+
+bool isEmpty()
+{
+	return (front == -1) || (front == SIZE -1);
+}
+
+bool isFull()
+{
+	return (rear ==  SIZE - 1);
+}
+
+void Insert(int element)
+{
+	front = 0;
+	rear = 0;
+	queue[rear] = element;
+}
+
+void Enqueue()
+{
+	int element;
+	printf("Enter an element: ");
+	scanf("%d", &element);
+	
+	if(front == -1 && rear == -1)
+	{
+		Insert(element);
+	}
+	
+	else if(isFull())
+	{
+		printf("Queue is full.\n");
+	}
+	
+	else
+	{
+		rear = rear + 1;
+		queue[rear] = element;
+	}
+	
+}
+
+void Dequeue()
+{
+	if(isEmpty)
+	{
+		printf("Queue is empty.\n");
+	}
+	
+	else
+	{
+		printf("Dequeued element: %d", queue[front]);
+		front = front + 1;
+	}
+}
+
+void Display()
+{
+	int i;
+	if(isEmpty())
+	{
+		printf("Queue is empty.\n");
+	}
+	else
+	{
+		for(i = front; i <= rear; i++)
+		{
+			printf("%d\t", queue[i]);
+		}
+		printf("\n");
+	}
+}
